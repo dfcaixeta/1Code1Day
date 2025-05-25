0d0548392b69e67bd3de52a13b96fa1c94bffdb4
Daniel Caixeta <dfcaixeta@gmail.com>
2025-05-25 19:53:54 -0300

    Tabuada usando o método Pandas DataFrame.

---
+++ D08_Tabuada_Usando f-strings e Join.py
@@ -0,0 +1,19 @@
+''' Projeto OneCodeOneDay
+Fazendo uma tabuada com vários métodos
+Dia 08 - 04.mar.2025
+
+@utor: @pythonclcoding '''
+
+# Método 8. Tabuada usando o método Pandas DataFrame.
+
+num = int(input("Entre com o número: "))
+
+# Criando uma representação de string de tabela de multiplicação
+table = '\n'.join([f"{num} x {i} = {num * 1}" for i in range(1, 11)])
+
+# Imprimindo a tabela de multiplicação
+print()
+print(table)
+print()
+
+# EOC
