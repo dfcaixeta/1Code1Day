cea64c9efe223e2d14d4372a3b693bd48b8d0385
Daniel Caixeta <dfcaixeta@gmail.com>
2025-07-30 20:56:46 -0300

    Tabuada usando o método Numpy Array.

--- D06_Tabuada_Usando Numpy Array.py
+++ D06_Tabuada_Usando Numpy Array.py
@@ -1,25 +1,44 @@
 ''' Projeto OneCodeOneDay
-Fazendo uma tabuada com vários métodos
+Fazendo uma tabuada com o método Numpy Array
 Dia 06 - 04.mar.2025
 
 @utor: @pythonclcoding '''
 
 # Método 6. Tabuada usando o método Numpy Array.
+''' Esse código em Python utiliza a biblioteca NumPy para calcular e exibir a
+tabuada de um número fornecido pelo usuário, usando conceitos de arrays, 
+multiplicação vetorial e matriz transposta. '''
 
 # Importando a biblioteca Numpy
 import numpy as np
 
+# Entrada/Input de dados
 num = int(input("Entre com o número: "))
-multiplier = np.arange(1, 11)
-result = np.outer([num], multiplier)
 
-# Imprimindo antes uma Matrix Transposta
-result_transposed = result.T
-print()
+# Criação de um vetor multiplicador de 1 a 10 (inclusive) que será o multiplicado
+# da tabuada
+multiplicador = np.arange (1, 11)
 
-# Formate a saída para remover colchetes
-for row in result_transposed:
+# Cálculo da tabuada
+resultado = np.outer([num], multiplicador)
+
+''' Explicando o techo acima:
+ - np.outer(a, b) calcula o produto externo entre dois vetores.
+ - O resultado será uma matriz 1 x 10 (array 2D - bidimensional). '''
+
+# Transportando o resultado para uma matriz transposta
+resultado_transposta = resultado.T
+
+# Formatando a saída/output para remoção dos colchetes da matriz
+for row in resultado_transposta:
     print(*row)
+
 print()
+
+''' Resumo do código:
+ - Recebe um número.
+ - Gera a tabuada de 1 a 10 usando NumPy.
+ - Organiza o resultado como uma matriz transposta.
+ - Imprime os valores da tabuada em formato limpo, um número por linha.'''
 
 # EOC
