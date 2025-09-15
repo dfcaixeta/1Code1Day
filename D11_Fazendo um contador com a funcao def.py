4b88653848b910b3967b22fef8565ef9bb239195
Daniel Caixeta <dfcaixeta@gmail.com>
2025-09-14 21:06:44 -0300

    Código contador com a função count_fun() que conta o número de vezes em que foi chamado.

--- D11_Fazendo um contador com a funcao def.py
+++ D11_Fazendo um contador com a funcao def.py
@@ -5,8 +5,8 @@ Dia 11 - 05.mar.2025
 
 @utor: @python.joy '''
 
-#def count_func(data = None):
-def count_func(data = {}):
+#def count_func(data = None): # Cada chamada sem argumento começa do zero e termina em 1.
+def count_func(data = {}): # Com a função null, inicia em 1 e faz os incrementos.
     if data is None:
         data = {}
     data['count'] = data.get('count', 0) + 1
