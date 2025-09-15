0ff0a5fde49272ef81d571fe56a69c2a525610db
Daniel Caixeta <dfcaixeta@gmail.com>
2025-09-14 21:58:39 -0300

    Gráfico de Pareto usando Matplotlib.

--- D13_Grafico_Pareto_Usando Python.py
+++ D13_Grafico_Pareto_Usando Python.py
@@ -6,6 +6,7 @@ Dia 13 - 25.mai.2025
 
 import matplotlib.pyplot as plt
 
+# Preparando os dados de entrada (Input).
 categorias = ['Categoria 1', 'Categoria 2', 'Categoria 3', 'Categoria 4']
 valores = [80, 20, 50, 30]
 
