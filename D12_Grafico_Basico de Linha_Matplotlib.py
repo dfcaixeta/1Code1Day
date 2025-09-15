2513c80478ee557581320adec89777a47e9ce112
Daniel Caixeta <dfcaixeta@gmail.com>
2025-09-14 21:54:23 -0300

    Gráfico básico de linha usando Matplotlib.

--- D12_Grafico_Basico de Linha_Matplotlib.py
+++ D12_Grafico_Basico de Linha_Matplotlib.py
@@ -7,16 +7,16 @@ Dia 12 - 25.mai.2025
 # Importando a biblioteca.
 import matplotlib.pyplot as plt
 
-# Dados de entrada.
+# Preparando os dados de entrada (Input).
 x = [1, 2, 3, 4, 5] # Eixo x
 y = [2, 3, 5, 7, 11] # Eixo y
 
 # Preparando o gráfico.
-plt.plot(x, y, marker = 'o', color='green')
-plt.title('Gráfico Básico de Linha')
-plt.xlabel('Eixo x')
-plt.ylabel('Eixo y')
-plt.grid(True)
+plt.plot(x, y, marker = 'o', color='green') # Plota os elementos do gráfico.
+plt.title('Gráfico Básico de Linha') # Plota o título do gráfico.
+plt.xlabel('Eixo x') # Plota o Eixo x.
+plt.ylabel('Eixo y') # Plot o Eixo y.
+plt.grid(True) # Plota o grid no gráfico.
 
 # Plotando o gráfico.
 plt.show()
