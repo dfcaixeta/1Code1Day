ae73f4e7ea7fd39296f751f685188740cdfae62d
Daniel Caixeta <dfcaixeta@gmail.com>
2026-01-10 13:09:39 -0300

    Tabuada usando estrutura de repetição while.

---
+++ Dia_04_Tabuada_usando_while/Dia_04_Tabuada_usando_while.md
@@ -0,0 +1,8 @@
+## 📌 Síntese do código
+
+Trata-se de um código que é um gerador de tabuadas usando estrutura de repetição while. Ele:
+
+- Pede ao usuário um número inteiro.
+- Inicializa um contador (i = 1).
+- Usa o laço `while` para calcular e mostrar a multiplicação do número por valores de 1 até 10.
+- Finaliza com uma linha em branco para melhorar a apresentação.
---
+++ Dia_04_Tabuada_usando_while/Dia_04_Tabuada_usando_while.py
@@ -0,0 +1,23 @@
+''' Projeto OneCodeOneDay
+Esse código em Python tem como finalidade gerar e exibir a tabuada de multiplicação 
+de um número informado pelo usuário utilizando a estrutura de repetição while.
+
+Fazendo uma tabuada usando o método while ... loop ...
+Dia 04 - 10.jan.2026
+
+@utor: @https://github.com/dfcaixeta '''
+
+# Método 1. Tabuada usando o Estrutura de repetição (loop) while ...
+
+# Entrada/Input de dado
+num = int(input('Entre com um número: '))
+i = 1
+
+# Estrutura de repetição com while.
+while i <= 10:
+    print(num, 'x', i, '=', (num * i))
+    i += 1
+
+print()
+
+# EOC
